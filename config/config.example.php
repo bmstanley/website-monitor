abc0c3b2851d9dfdee37bdb77de43e3423ed2040
Brian Stanley <bmstanley87@gmail.com>
2018-05-18 21:19:47 -0400

    Add example config

---
+++ config/config.example.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'request' => [
+        'url' => ['full_website_address']
+    ],
+    'email' => [
+        'fromAddress' => 'from_email_address',
+        'fromPassword' => 'top_secret_password',
+        'recipients' => ['to_email_address_1']
+    ],
+];
