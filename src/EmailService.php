919608b33e79b92c85da15d6a46710f8588f7247
Brian Stanley <bmstanley87@gmail.com>
2018-05-20 18:12:28 -0400

    Refactor classes into src folder

--- src/EmailService.php
+++ src/EmailService.php
@@ -1,54 +1,62 @@
 <?php
 
-require_once "vendor/autoload.php";
+namespace bmstanley;
 
-class Email extends BaseClass
+final class EmailService
 {
     /**
      * Secure protocol to be used to connect to email host
+     *
      * @var string
      */
     private $smtpProtocol = 'ssl';
 
     /**
      * Email host SMTP server
+     *
      * @var string
      */
     private $smtpHost = 'smtp.gmail.com';
 
     /**
      * Enable SMTP authentication
+     *
      * @var boolean
      */
     private $smtpAuth = true;
 
     /**
      * Email host SMTP port
+     *
      * @var integer
      */
     private $smtpPort = 465;
 
     /**
      * Email from address
+     *
      * @var string
      */
     private $from;
 
     /**
      * Email from plain name
+     *
      * @var string
      */
     private $fromName = 'System Monitoring';
 
     /**
      * Email from password
+     *
      * @var string
      */
     private $password;
 
     /**
      * Random call to action phrase for email body
-     * @var [type]
+     *
+     * @var array
      */
     private $emailBody = [
         'What are you waiting for? Go fix it!',
@@ -59,17 +67,21 @@ class Email extends BaseClass
 
     /**
      * Create and send an email
-     * @param array $recipients One or more email recipients
+     *
+     * @param array  $config        Configuration settings
+     *      $config = [
+     *          'fromAddress' => (string) The email address from which the email will be sent
+     *          'fromPassword' => (string) The password for the fromAdress account
+     *          'recipients' => (array) The recipient email addresses
+     *      ]
      * @param string $customMessage Custom message to be inserted into the body of the email
      */
-    public function __construct(array $recipients, string $customMessage = null)
+    public function __construct(array $config, array $recipients, string $customMessage = null)
     {
-        parent::__construct();
+        $this->from = $config['email']['fromAddress'];
+        $this->password = $config['email']['fromPassword'];
 
-        $this->from = $this->config['email']['fromAddress'];
-        $this->password = $this->config['email']['fromPassword'];
-
-        $mail = new PHPMailer;
+        $mail = new \PHPMailer;
         $mail->isSMTP();
         $mail->Host = $this->smtpHost;
         $mail->SMTPAuth = $this->smtpAuth;
@@ -81,7 +93,7 @@ class Email extends BaseClass
         $mail->From = $this->from;
         $mail->FromName = $this->fromName;
 
-        foreach ($recipients as $recipient) {
+        foreach ($config['email']['recipients'] as $recipient) {
             $mail->addAddress($recipient);
         }
 
@@ -90,15 +102,15 @@ class Email extends BaseClass
         $mail->Subject = "Website Unresponsive";
         $mail->Body = "<h1>Website Unresponsive</h1>";
         if (!is_null($customMessage)) {
-            $mail->Body .= "<H3>".$customMessage."</H3>";
+            $mail->Body .= "<H3>" . $customMessage . "</H3>";
         }
-        $mail->Body .= "<p>".$this->emailBody[array_rand($this->emailBody)]."</p>";
+        $mail->Body .= "<p>" . $this->emailBody[array_rand($this->emailBody)] . "</p>";
 
         if (!$mail->send()) {
             echo $mail->ErrorInfo;
             return false;
         }
-        echo "email sent successfully";
+        echo "Email sent successfully\n";
         return true;
     }
 }
